86cc1804751b44527980565e8918682436a0d7d0
Daniel <danbg31@gmail.com>
2022-10-03 21:08:00 -0600

    calendario dias

--- src/app/pages/calendario/calendario.component.ts
+++ src/app/pages/calendario/calendario.component.ts
@@ -4,7 +4,10 @@ import { CalendarOptions } from '@fullcalendar/angular';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
+import { StorageService } from 'src/app/storage.service';
 
+
+declare let $: any;
 
 @Component({
   selector: 'app-calendario',
@@ -13,14 +16,57 @@ import listPlugin from '@fullcalendar/list';
 })
 export class CalendarioComponent implements OnInit {
 
-  Events = [];
+  Events = [
+
+    {
+      id: 'a',
+      title: 'DaniGames',
+      start: '2022-10-04'
+    }
+
+  ];
   calendarOptions!: CalendarOptions;
 
-  constructor() { 
+  user : any;
+
+  constructor(private localSotage: StorageService) { 
+
+    this.user = JSON.parse(localSotage.getData('user'));
+
    }
 
-  onDateClick(res: { dateStr: string; }) {
-    alert('You clicked on : ' + res.dateStr)
+   
+
+  onDateClick(res : any) {
+    if(!res.allDay){
+      let date = res.date.toJSON().split('.')[0]
+      console.log(date)
+
+      $("#myModal").modal("show");
+      $(".modal-title").text("");
+      $(".modal-title").text("Add Event at : " + date);
+
+      // this.Events.push(
+      //   {
+      //     id : String(this.user.email) , 
+      //     title: 'Dani', 
+      //     start: date
+      //   })
+
+      this.Events.push(
+        {
+          id: 'a',
+          title: 'DaniGames',
+          start: '2022-10-05'
+        }
+      )
+
+      console.log(this.Events)
+    }
+
+    
+
+    
   }
 
   ngOnInit() {
